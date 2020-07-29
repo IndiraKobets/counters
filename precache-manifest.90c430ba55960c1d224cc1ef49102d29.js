32e628d71e0974be5820d17d95542d063422b440
indiraaridni <indiraaridni@mail.ru>
2020-07-28 22:24:15 -0400

    Updates

---
+++ precache-manifest.90c430ba55960c1d224cc1ef49102d29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c467dd1c74361cd8c2de5d35dce611a2",
+    "url": "/counters/index.html"
+  },
+  {
+    "revision": "918c85499d2c63d61292",
+    "url": "/counters/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "74b767eb3d23ae147310",
+    "url": "/counters/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "918c85499d2c63d61292",
+    "url": "/counters/static/js/2.01e11b8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counters/static/js/2.01e11b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b767eb3d23ae147310",
+    "url": "/counters/static/js/main.b9c8db23.chunk.js"
+  },
+  {
+    "revision": "a64d93fc1d448c302c58",
+    "url": "/counters/static/js/runtime-main.95d00a74.js"
+  }
+]);
